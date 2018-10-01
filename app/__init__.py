25581d31ef69639fccba3ba035c9eca0911568b4
Moses Kinara <kinaramose@gmail.com>
2018-10-02 00:30:59 +0300

    Restructured working directory

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
-
 from flask import Flask
 from flask_restful import Api
-from app.apiv1.views.orders import Orders, SpecificOrder, AcceptedOrders,DeclineOrder,DeclinedOrders,PendingOrders,CompleteOrder,CompletedOrders
+from app.apiv1.orders.views import Orders, SpecificOrder, AcceptedOrders,DeclineOrder,DeclinedOrders,PendingOrders,CompleteOrder,CompletedOrders
 from instance.config import app_config
 
 
@@ -9,7 +8,7 @@ def create_app(config_name='development'):
 	app = Flask(__name__, instance_relative_config=True)
 	app.config.from_object(app_config["development"])
 	app.config.from_pyfile('config.py')
-	from .apiv1.views import orders_bp as orders_blueprint
+	from .apiv1.orders import orders_bp as orders_blueprint
 	api = Api(orders_blueprint)
 	app.register_blueprint(orders_blueprint, url_prefix='/api/v1')
 	api.add_resource(Orders, '/orders')
