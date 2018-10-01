84599dc48976443500e9fcb5fe1292b617dd0e0d
Moses Kinara <kinaramose@gmail.com>
2018-10-02 01:29:16 +0300

    Created tables for db

---
+++ app/apiv2/database/connect.py
@@ -0,0 +1,36 @@
+import psycopg2
+import os 
+from .create_tables import queries, to_drop
+from flask import current_app
+
+#create a connection
+def create_conn():
+    url = current_app.config.get('POSTGRES_DATABASE_URI')
+    conn = psycopg2.connect(url)
+    return conn
+
+#Create Database tables for tests
+def create_db(): 
+    conn = create_conn()
+    curr = conn.cursor()
+    conn.autocommit = True
+    for query in queries:
+        curr.execute(query)
+
+    curr.close()
+    conn.commit()
+    return conn
+
+
+# drop table for tests
+def drop_db():
+    conn = create_conn()
+    curr = conn.cursor()
+    try: 
+        for query in to_drop:
+            curr.execute(query)
+
+        curr.close()
+        conn.commit()
+    except:
+        print("failed")
